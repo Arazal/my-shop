1e4da0ae03b7d44885517d09039753da1f389e58
Arsen4ik <46644855+Arsen4ik@users.noreply.github.com>
2019-08-07 09:00:46 +0300

    fidex app.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
+
 const config = {
     
     apiKey: "AIzaSyDh231hGge-kYJYPyxGe0PahpbUe7FYsnw",
@@ -11,6 +12,33 @@ const config = {
     storageBucket: "",
     messagingSenderId: "586121035116",
     appId: "1:586121035116:web:cdd0b9f343d9c4c1"
+}
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({
+             displayName,
+             email,
+             createdAt,
+             ...additionalData   
+            })
+        } catch (error) {
+            console.log('error', error.message)
+        }
+    }
+
+    console.log(firestore.doc('users/111111'))
+    return userRef
 }
 
 firebase.initializeApp(config)
